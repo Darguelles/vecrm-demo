18ceec7c714a6a55f225b5f7db436a929974117f
Arguelles Rojas, Diego J <diego.arguellesrojas@one.verizon.com>
2017-01-02 11:47:19 -0500

    Search bar component created. Missing set action attributes for buttons and links

--- src/app/components/search.bar.component.ts
+++ src/app/components/search.bar.component.ts
@@ -1,1 +1,10 @@
+import {Component, Input, Output} from '@angular/core';
 
+@Component({
+  selector: 'search-bar',
+  templateUrl: 'search.bar.component.html',
+})
+export class SearchBarComponent {
+  // TODO put attribute list to <a> element (href)
+  // TODO define an action attribute for the search button
+}
