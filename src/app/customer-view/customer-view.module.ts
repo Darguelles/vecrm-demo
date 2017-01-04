c946aa3b8ae23ec55c5c42b8137f1fb892c77b3f
Arguelles Rojas, Diego J <diego.arguellesrojas@one.verizon.com>
2017-01-04 14:52:28 -0500

    Bar chart is ready for use

--- src/app/customer-view/customer-view.module.ts
+++ src/app/customer-view/customer-view.module.ts
@@ -9,12 +9,14 @@ import {CardComponent} from '../components/card.component.ts'
 import {OrderContainerComponent} from '../components/order.container.component.ts'
 import {SearchBarComponent} from '../components/search.bar.component'
 import {CircleReportComponent} from '../report/circle.report.component'
+import {BarChartComponent} from '../report/bar.chart.component'
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 
 @NgModule({
-  imports: [BrowserModule, RouterModule, CustomerViewRoutes],
+  imports: [BrowserModule, RouterModule, CustomerViewRoutes, ChartsModule],
   exports: [],
-  declarations: [CustomerViewComponent, Grid, CardComponent, OrderContainerComponent, SearchBarComponent, CircleReportComponent],
+  declarations: [CustomerViewComponent, Grid, CardComponent, OrderContainerComponent, SearchBarComponent, CircleReportComponent, BarChartComponent],
   providers: [CustomerViewService],
   schemas: [NO_ERRORS_SCHEMA]
 })
