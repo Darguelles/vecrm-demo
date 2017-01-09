8362a5cd8e7616d698b3bc07e76bee924a739497
Arguelles Rojas, Diego J <diego.arguellesrojas@one.verizon.com>
2017-01-09 14:58:24 -0500

    Optimized imports

--- src/app/customer-view/customer-view.module.ts
+++ src/app/customer-view/customer-view.module.ts
@@ -5,16 +5,12 @@ import {RouterModule}  from '@angular/router';
 import {ChartsModule} from 'ng2-charts/ng2-charts';
 import {CustomerViewRoutes}   from './customer-view.routes';
 import {CustomerViewService}   from './services/customer-view.service';
-import {Grid}   from '../grid/grid';
-import {OrderContainerComponent} from '../components/order.container.component.ts'
-import {SearchBarComponent} from '../components/search.bar.component'
 import {CircleReportComponent} from '../report/circle.report.component'
 import {BarChartComponent} from '../report/bar.chart.component'
 import {EventReportComponent} from '../report/event.report.component';
-import {ButtonContainerComponent} from '../components/button.container.component';
 import {ReqOpenedReportComponent} from '../report/req.opened.report.component';
 import {PieChartComponent} from '../report/pie.chart.component';
-import {ChatterComponent} from '../components/chatter.component';
+import {ChatterComponent} from '../components/chatter/chatter.component';
 import {PieChart2Component} from '../report/pie.chart2.component';
 import {GenericComponentModule} from "../components/generic-component.module";
 
@@ -23,8 +19,7 @@ import {GenericComponentModule} from "../components/generic-component.module";
   imports: [BrowserModule, RouterModule, CustomerViewRoutes, ChartsModule,
     GenericComponentModule],
   exports: [],
-  declarations: [CustomerViewComponent, CircleReportComponent,
-    BarChartComponent, EventReportComponent, ReqOpenedReportComponent, PieChartComponent, ChatterComponent, PieChart2Component],
+  declarations: [CustomerViewComponent, CircleReportComponent, BarChartComponent, EventReportComponent, ReqOpenedReportComponent, PieChartComponent, ChatterComponent, PieChart2Component],
   providers: [CustomerViewService],
   schemas: [NO_ERRORS_SCHEMA]
 })
