918b1c3044ae301c44420f8b300c6541c9a758ad
Arguelles Rojas, Diego J <diego.arguellesrojas@one.verizon.com>
2017-01-09 14:57:44 -0500

    Components organized in folders

---
+++ src/app/components/action-bar/action-bar.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'action-bar',
+  templateUrl: 'action-bar.component.html',
+})
+export class ActionBarComponent {
+  // TODO put attribute list to <a> element (href)
+  // TODO define an action attribute for the search button
+}
--- src/app/components/generic-component.module.ts
+++ src/app/components/generic-component.module.ts
@@ -1,9 +1,9 @@
 import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {BrowserModule}  from '@angular/platform-browser';
-import {CardComponent} from "./card.component";
-import {OrderContainerComponent} from "./order.container.component";
-import {SearchBarComponent} from "./search.bar.component";
-import {ButtonContainerComponent} from "./button.container.component";
+import {CardComponent} from "./card/card.component";
+import {OrderContainerComponent} from "./order/order.container.component";
+import {SearchBarComponent} from "./search-bar/search.bar.component";
+import {ButtonContainerComponent} from "./button-container/button.container.component";
 import {CommonModule}        from '@angular/common';
 import {Grid} from "../grid/grid";
 
