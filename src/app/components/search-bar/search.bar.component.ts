ba420ad289c3a45773258bd90e93ddbe7f202cd7
Arguelles Rojas, Diego J <diego.arguellesrojas@one.verizon.com>
2017-01-20 12:23:38 -0500

    Search bar nos uses percentaje in distribution and is aligned inside the div parent

--- src/app/components/search-bar/search.bar.component.ts
+++ src/app/components/search-bar/search.bar.component.ts
@@ -7,4 +7,7 @@ import {Component, Input, Output} from '@angular/core';
 export class SearchBarComponent {
   // TODO put attribute list to <a> element (href)
   // TODO define an action attribute for the search button
+  @Input() widhtSearchBar: string = '80%';
+  @Input() widhtButtonBar: string = '20%';
+
 }
