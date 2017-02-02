274d47a0de20931d646a3586d0f22229dc0b2b71
darguelles.rojas91@gmail.com <cuganina>
2017-02-02 01:11:07 -0500

    Sticky header ready

--- src/app/layout-free/layout-free.component.ts
+++ src/app/layout-free/layout-free.component.ts
@@ -1,19 +1,20 @@
-import { Component, ElementRef } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { AppConfig } from '../app.config';
+import {Component, ElementRef, HostListener, Inject} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+import {AppConfig} from '../app.config';
+import {DOCUMENT} from '@angular/platform-browser';
 
 declare var jQuery: any;
 
 @Component({
-    selector: 'layout-free',
-    providers: [],
-    templateUrl: './layout-free.html',
-    host: {
-        '[class.nav-static]': 'config.state["nav-static"]',
-        '[class.chat-sidebar-opened]': 'chatOpened',
-        '[class.app]': 'true',
-        id: 'app'
-    }
+  selector: 'layout-free',
+  providers: [],
+  templateUrl: './layout-free.html',
+  host: {
+    '[class.nav-static]': 'config.state["nav-static"]',
+    '[class.chat-sidebar-opened]': 'chatOpened',
+    '[class.app]': 'true',
+    id: 'app'
+  }
 })
 export class LayoutFreeComponent {
   config: any;
@@ -23,13 +24,31 @@ export class LayoutFreeComponent {
   router: Router;
   chatOpened: boolean = false;
 
+  headerClass:string = 'dashboard-header-top'
+
   constructor(config: AppConfig,
               el: ElementRef,
-              router: Router) {
+              router: Router
+    , @Inject(DOCUMENT) private document: Document) {
     // Raphael.prototype.safari = function(): any { return; };
     this.el = el;
     this.config = config.getConfig();
     this.configFn = config;
     this.router = router;
   }
-     }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    let number = this.document.body.scrollTop;
+    if (number == 0) {
+      console.log('A')
+      this.headerClass = 'dashboard-header-top'
+
+    } else {
+      console.log('B')
+      this.headerClass = 'dashboard-header'
+    }
+  }
+
+
+}
--- src/app/layout-free/navbar/navbar.component.ts
+++ src/app/layout-free/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, HostListener, Inject} from '@angular/core';
 
-import { UserInfo } from './user-info';
-import { Application } from './application';
+import {UserInfo} from './user-info';
+import {Application} from './application';
 
-import { UserInfoService } from './user-info.service';
+import {DOCUMENT} from '@angular/platform-browser';
+
+import {UserInfoService} from './user-info.service';
 
 @Component({
   selector: '[navbar]',
@@ -18,16 +20,36 @@ export class NavbarComponent implements OnInit {
   fullName: string;
   userName: string;
 
-  constructor(private userInfoService: UserInfoService) {
+  headerClass:string = 'button-hide-top'
+  checkboxClass:string = 'checkbox-group-display-top'
+  searchBarClass:string = 'search-bar-top'
+  iconLogoClass:string = 'icon-logo-top'
+
+  constructor(private userInfoService: UserInfoService, @Inject(DOCUMENT) private document: Document) {
   }
 
-  ngOnInit() {
-    this.getUserInfo();
+  ngOnInit() {}
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    let number = this.document.body.scrollTop;
+    if (number == 0) {
+      this.headerClass   = 'button-hide-top'
+      this.checkboxClass = 'checkbox-group-display-top'
+      this.searchBarClass = 'search-bar-top'
+      this.iconLogoClass = 'icon-logo-top'
+
+    } else {
+      this.checkboxClass = 'checkbox-group-display'
+      this.headerClass = 'button-hide'
+      this.searchBarClass = 'search-bar'
+      this.iconLogoClass = 'icon-logo'
+    }
   }
 
   getUserInfo(): void {
 
-   
+
   }
 
   public logout() {
